a25dad976c2be47fa80c5cc4d723b239c3cbc98f
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-13 16:03:20 +0100

    somma console.log

--- Snack-1/js/script.js
+++ Snack-1/js/script.js
@@ -7,9 +7,16 @@ console.log('JS OK');
 
 //Chiedere all'utente di inserire un numero per 10 volte (con prompt)
 
+let sum = 0; 
 
 for (let i = 0; i < 10; i++) {
     let numbers = parseInt(prompt('Inserisci un numero').trim());
 
     console.log(numbers);
+
+    sum = sum + numbers;
+
+    console.log(sum);
+
+
 }
