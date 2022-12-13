315e99c1595042d50a41ce025e06481f71c5cf6a
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-13 17:38:20 +0100

    prova verifica nominativi

--- Snack-2/js/script.js
+++ Snack-2/js/script.js
@@ -4,9 +4,14 @@ console.log('JS OK');
 // l’utente inserisce il suo nome tramite un input.comunicagli se può partecipare o no alla festa.
 
 
-const btnUser = document.getElementById('btnUser');
+// Creo lista degli invitati 
 
 const guestName = ["Romolo", "Numa Pompilio", "Tullo Ostilio", "Anco Marzio", "Tarquinio Prisco","Servio Tullio", "Tarquinio il Superbo"];
+
+
+// Prendo elementi dalla pagina 
+
+const btnUser = document.getElementById('btnUser');
 
 const message = document.getElementById('message');
 
@@ -17,12 +22,51 @@ let contentMessage = '';
 
 btnUser.addEventListener("click", function(){
 
+    // Recupero il valore dall'input, nome inserito dall'utente 
+
     const userName = document.getElementById('userName').value;
+    
     let userAllowed = false; 
+
+
+    // Controllo il nome inserito tra gli invitati 
+
+    for (let i = 0; i < guestName.length && !userAllowed; i++) {
+
+        const currentName = guestName[i];
+        console.log(currentName, guestName);
+
+        
+        if (currentName === guestName) {
+
+           userAllowed = true;
+
+           console.log('userAllowed');
+
+        }
+
+    
+
+
+    }
+
+    // Monto il messaggio
+
+    if (userAllowed) {
+        
+        contentMessage = `<p>"Il tuo invito è confermato!"</p>`;
+
+
+        } else {
+            contentMessage = `<p>"Non sei stato invitato alla festa del Grande Gatsby, ci dispiace."</p>`;
+        }
+
+
+        console.log(contentMessage);
 
   
     
-        // Stampo in pagina
+    // Stampo in pagina
      
     message.innerHTML = contentMessage;
 
