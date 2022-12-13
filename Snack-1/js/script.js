21065f428fe8b07cf099fbcbd8009256044163a0
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-13 15:44:27 +0100

    ciclo for 10

---
+++ Snack-1/js/script.js
@@ -0,0 +1,15 @@
+console.log('JS OK');
+
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. (prompt)
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+//Chiedere all'utente di inserire un numero per 10 volte (con prompt)
+
+
+for (let i = 0; i < 10; i++) {
+    let numbers = parseInt(prompt('Inserisci un numero').trim());
+
+    console.log(numbers);
+}
